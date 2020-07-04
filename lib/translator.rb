d26db7cb0f15f5862b20221da911d64901ecc37c
Jina Strickland <jina30@gmail.com>
2020-07-04 22:52:48 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,33 @@
-# require modules here
+require 'pry'
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  file = YAML.load_file('./lib/emoticons.yml')
+  file.map do | key, value |
+binding.pry
+  {
+      "#{key}" =>  {
+      :english => value[0],
+      :japanese => value[1]
+        }
+      }
+file.map { |k, v| [k, v]}.to_h
+
+has = hash[array.collect { |item| [item, "value"]}]
+
+end
+binding.pry
+file
+
+#binding.pry
 end
 
+
 def get_japanese_emoticon
-  # code goes here
+#binding.pry
 end
 
 def get_english_meaning
-  # code goes here
+
 end
+#binding.pry
